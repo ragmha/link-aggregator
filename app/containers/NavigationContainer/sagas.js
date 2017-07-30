6e003a366a1f89d74908242b2b0300482cfcad6a
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-30 21:01:31 +0300

    storing data with redux-sag

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,7 +1,8 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { REQUEST_TOPICS } from './constants';
 import { takeLatest } from 'redux-saga';
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
+import { requestTopicsSucceeded, requestTopicsFailed } from './actions';
 
 export function fetchTopicsFromServer() {
   return fetch('http://localhost:3000/api/topics').then(response =>
@@ -10,8 +11,12 @@ export function fetchTopicsFromServer() {
 }
 
 function* fetchTopics() {
-  const topics = yield call(fetchTopicsFromServer);
-  console.log('TOPIC FROM THE SERVER', topics);
+  try {
+    const topics = yield call(fetchTopicsFromServer);
+    yield put(requestTopicsSucceeded(topics));
+  } catch (e) {
+    yield put(requestTopicsFailed(e.message));
+  }
 }
 
 // Individual exports for testing
