89a646d812394e80f4df379759315ec47173133d
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-30 20:55:59 +0300

    fetch data with redux-saga ;)

--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,12 +1,22 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 import { REQUEST_TOPICS } from './constants';
 import { takeLatest } from 'redux-saga';
+import { call } from 'redux-saga/effects';
+
+export function fetchTopicsFromServer() {
+  return fetch('http://localhost:3000/api/topics').then(response =>
+    response.json()
+  );
+}
+
+function* fetchTopics() {
+  const topics = yield call(fetchTopicsFromServer);
+  console.log('TOPIC FROM THE SERVER', topics);
+}
 
 // Individual exports for testing
 export function* fetchTopicsSaga() {
-  yield* takeLatest(REQUEST_TOPICS, () => {
-    console.log('LOADED BY SAGA');
-  });
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
