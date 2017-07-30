cbf9529f5eda1bc6120e36eed9badaca7736c1de
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-30 22:31:39 +0300

    storing selected topic in the store

--- app/containers/NavigationContainer/actions.js
+++ app/containers/NavigationContainer/actions.js
@@ -7,7 +7,8 @@
 import {
   REQUEST_TOPICS,
   REQUEST_TOPICS_SUCCEEDED,
-  REQUEST_TOPICS_FAILED
+  REQUEST_TOPICS_FAILED,
+  SELECT_TOPIC
 } from './constants';
 
 export function requestTopics() {
@@ -27,5 +28,12 @@ export function requestTopicsFailed(message) {
   return {
     type: REQUEST_TOPICS_FAILED,
     message
+  };
+}
+
+export function selectTopic(topic) {
+  return {
+    type: SELECT_TOPIC,
+    topic
   };
 }
